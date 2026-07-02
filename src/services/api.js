8e9a19fb432c7bc398ef95513c21b5353ccc3a0a
IgorCezarAlmeida <igorcezar205@gmail.com>
2026-07-01 22:53:58 -0300

    deploy

--- src/services/api.js
+++ src/services/api.js
@@ -5,7 +5,7 @@ const baseURL = import.meta.env.VITE_API_BASE_URL || ''
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
-    timeout: 60000,
+    timeout: 10000,
 })
 
 api.interceptors.request.use(
